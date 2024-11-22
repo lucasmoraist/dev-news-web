64c00d70282c7cc975407547e993c7621d24d886
lucasmoraist <luksmnt1101@gmail.com>
2024-11-22 14:35:35 -0300

    feat: implement dynamic rendering of posts in the main layout

--- src/js/view/displayPosts.js
+++ src/js/view/displayPosts.js
@@ -3,15 +3,17 @@ import { findAll } from "../api/post/findAll.js";
 import { formatedDate } from "../functions/formatedDate.js";
 
 findAll().then((response) => {
-  let postHtml = document.querySelector(".card-largo-wrapper");
-
   var posts = response.content;
+
+  let cardLargoHtml = document.querySelector(".card-largo-wrapper");
+  let cardPequenoHtml = document.querySelector(".card-pequeno-wrapper");
+  let cardBigHtml = document.querySelector(".card-big-container");
 
   posts.slice(0, 1).forEach((post) => {
     let id = post.id;
 
     getImage(id).then((image) => {
-      postHtml.innerHTML += `
+      cardLargoHtml.innerHTML += `
         <article class="card-largo">
           <div class="conteudo">
             <div>
@@ -39,6 +41,79 @@ findAll().then((response) => {
           />
         </article>
     `;
+    });
+  });
+
+  posts.slice(1, 4).forEach((post) => {
+    let id = post.id;
+
+    getImage(id).then((image) => {
+      cardPequenoHtml.innerHTML += `
+        <article class="card-pequeno">
+          <img
+            src=${image}
+            alt="Post preview image 2"
+            aria-hidden="true"
+          />
+          <div class="conteudo">
+            <div>
+              <h3>${post.title}</h3>
+              <p>
+                ${post.content.substring(0, 193)}...
+              </p>
+            </div>
+
+            <div>
+              <span class="data-postagem">${formatedDate(post.updatedAt)}</span>
+              <a
+                aria-label="Ler mais"
+                href="#"
+                class="ver-mais"
+                title="Leia mais sobre esse post"
+                >Read more</a
+              >
+            </div>
+          </div>
+        </article>
+      `;
+    });
+  });
+
+  posts.slice(4, 5).forEach((post) => {
+    let id = post.id;
+
+    getImage(id).then((image) => {
+      cardBigHtml.innerHTML += `
+        <article class="card-big">
+            <div class="conteudo">
+              <div>
+                <h3>${post.title}</h3>
+                <p>
+                ${post.content.substring(0, 193)}...
+
+                </p>
+              </div>
+
+              <div>
+                <span class="data-postagem">${formatedDate(
+                  post.updatedAt
+                )}</span>
+                <a
+                  aria-label="Ler mais"
+                  href="#"
+                  class="ver-mais"
+                  title="Leia mais sobre esse post"
+                  >Read more</a
+                >
+              </div>
+            </div>
+            <img
+              src=${image}
+              alt="Big post cover image"
+              aria-hidden="true"
+            />
+          </article>
+      `;
     });
   });
 });
