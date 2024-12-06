1d596949da07d1548a49d293140c24c83589aedb
lucasmoraist <luksmnt1101@gmail.com>
2024-12-06 08:47:20 -0300

    feat: enhance Profile page with user details and post listings, including input fields for email and password

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,9 +1,12 @@
 import { getUser } from "@/api/user/getUser";
 import { IUserResponse } from "@/interface/getUser.interface";
 import { useEffect, useState } from "react";
+import style from "./profile.module.scss";
+import { useImage } from "@/hooks/useImage";
 
 export function Profile() {
   const [user, setUser] = useState<IUserResponse>();
+  const [inputActive, setInputActive] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
@@ -16,10 +19,45 @@ export function Profile() {
 
   return (
     <div>
-      <h1>Profile</h1>
-      <p>{user?.name}</p>
-      <p>{user?.email}</p>
-      <p>{user?.password}</p>
+      <div className={style.banner}></div>
+      <div>
+        <h2>{user?.name}</h2>
+      </div>
+
+      <div>
+        <label htmlFor="">
+          <span>Email: </span>
+          <input
+            type="email"
+            value={user?.email}
+            disabled={inputActive ? true : false}
+          />
+        </label>
+        <label htmlFor="">
+          <span>Senha: </span>
+          <input
+            type="password"
+            value={user?.password}
+            disabled={inputActive ? true : false}
+          />
+        </label>
+      </div>
+
+      <div>
+        <h3>Minhas postagens</h3>
+        {user?.posts.map((p) => {
+          const image = useImage(p.imageBanner);
+          return (
+            <div key={p.id}>
+              <img src={image} alt="" />
+              <h4>{p.title}</h4>
+              <p>{p.updatedAt}</p>
+              <i className="bi bi-pencil-square"></i>
+              <i className="bi bi-trash"></i>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
