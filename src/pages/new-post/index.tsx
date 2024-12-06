8872c5f1227e083a7fd27a1ebaf54225191700bc
lucasmoraist <luksmnt1101@gmail.com>
2024-12-06 09:17:56 -0300

    feat: refactor NewPost component to use Banner, BackButton, and TextEditor components

--- src/pages/new-post/index.tsx
+++ src/pages/new-post/index.tsx
@@ -1,48 +1,13 @@
-import "react-quill/dist/quill.snow.css";
-import ReactQuill from "react-quill";
 import { useState } from "react";
 import style from "./new-post.module.scss";
-import { Link } from "react-router-dom";
+import { Banner } from "./components/banner";
+import { BackButton } from "./components/back-button";
+import { TextEditor } from "./components/text-editor";
 
 export function NewPost() {
-  const modules = {
-    toolbar: [
-      [{ header: [1, 2, false] }],
-      ["bold", "italic", "underline", "strike", "blockquote"],
-      [
-        { list: "ordered" },
-        { list: "bullet" },
-        { indent: "-1" },
-        { indent: "+1" },
-      ],
-      ["link", "image"],
-      ["clean"],
-    ],
-  };
-
-  const formats = [
-    "header",
-    "bold",
-    "italic",
-    "underline",
-    "strike",
-    "blockquote",
-    "list",
-    "bullet",
-    "indent",
-    "link",
-    "image",
-  ];
-
   const [title, setTitle] = useState<string>("");
   const [content, setContent] = useState<string>();
   const [file, setFile] = useState<File>();
-
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files[0]) {
-      setFile(e.target.files[0]);
-    }
-  };
 
   const handleSave = () => {
     const data = {
@@ -56,22 +21,9 @@ export function NewPost() {
 
   return (
     <div className={style.container}>
-      <div className={style.voltar}>
-        <Link to={"/"}>Voltar</Link>
-      </div>
-
-      {file ? (
-        <img
-          src={URL.createObjectURL(file)}
-          alt="Banner"
-          className={style.imageBanner}
-        />
-      ) : (
-        <div className={style.banner}>
-          <span>Escolha uma imagem de banner</span>
-          <input type="file" onChange={handleFileChange} />
-        </div>
-      )}
+      <BackButton />
+      <Banner file={file} setFile={setFile} />
+      
       <div className={style.header}>
         <input
           type="text"
@@ -86,16 +38,7 @@ export function NewPost() {
         </button>
       </div>
 
-      <div className={style.editingArea}>
-        <ReactQuill
-          theme="snow"
-          modules={modules}
-          formats={formats}
-          value={content}
-          onChange={setContent}
-          style={{ height: "100%" }}
-        />
-      </div>
+      <TextEditor content={content} setContent={setContent} />
     </div>
   );
 }
